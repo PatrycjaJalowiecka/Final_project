ab4ca5eaaea546b68b341847656e4cc9583535a7
Patrycja Jałowiecka <pat.jalowiecka@gmail.com>
2021-03-24 23:26:46 +0100

    config.py

---
+++ blog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask  
+app = Flask(__name__)      
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from blog import routes, models
+
+
+##def make_shell_context():
+  ##return {"db": db, "Entry": models.Entry}
+
---
+++ blog/routes.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template, redirect
+from blog import app
+from blog.models import Entry
+
+ 
+@app.route("/")
+def blog_base():
+    return render_template("base.html")
+
+@app.route("/")
+def index():
+      all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+   return render_template("homepage.html", all_posts=all_posts)
+
+##@app.shell_context_processor
+
+
+##if __name__ == "__main__":
+  ##  app.run(debug=True)
